4230402feb9e32710f1a08823aa9819cab2dbed6
xcqq <xc.zhang@outlook.com>
2023-10-29 21:31:09 +0800

    Add mid filter to improve repeated accuracy

--- pressure_bed.ino
+++ pressure_bed.ino
@@ -9,7 +9,7 @@
 #define ADC_DOUT PA7
 
 // pressure sensor configure
-#define SENSOR_MAX_WEIGHT 5000
+#define SENSOR_MAX_WEIGHT 50000
 #define SENSOR_MAX_VOLT 1 // mv
 #define SENSOR_REVERSE 1
 
@@ -18,41 +18,60 @@
 #define ADC_MAX_VALUE (1 << 24)
 #define ADC_PGA 128 // just for reference
 
+// filter configure
+#define FILTER_WINDOW_SIZE 20
+
 // edge detection configure
 #define WINDOW_SIZE 500
 #define THRESHOLD_STATIC 250 // g
-#define EDGE_CONTINUOUS 1 // could make it more stable
 
 typedef struct {
-    int32_t data[WINDOW_SIZE];
-    int32_t sum;
-    int32_t mean;
-    int32_t index;
+    int64_t *data;
+    int64_t *sort_data;
+    uint32_t size;
+    int64_t sum;
+    int64_t mean;
+    int64_t mid;
+    int64_t index;
 } window_data_t;
 
 window_data_t window_data;
+window_data_t filter_window_data;
 
-void window_data_init(window_data_t *wd)
+
+int compare(const void *a, const void *b) {
+    return (*(int64_t*)a - *(int64_t*)b);
+}
+
+void window_data_init(window_data_t *wd, uint32_t size)
 {
     wd->sum = 0;
     wd->mean = 0;
     wd->index = 0;
-    memset(wd->data, 0, sizeof(wd->data));
+    wd->mid = 0;
+    wd->size = size;
+    wd->data = (int64_t *)calloc(size, sizeof(*wd->data));
+    wd->sort_data = (int64_t *)calloc(size, sizeof(*wd->sort_data));
 }
 
-void window_data_add(window_data_t *wd, int32_t data)
+void window_data_add(window_data_t *wd, int64_t data)
 {
     wd->sum -= wd->data[wd->index];
     wd->data[wd->index] = data;
     wd->sum += data;
-    wd->mean = wd->sum / WINDOW_SIZE;
-    wd->index = (wd->index + 1) % WINDOW_SIZE;
+    wd->mean = wd->sum / (int64_t)wd->size;
+    wd->index = (wd->index + 1) % wd->size;
+}
+
+void window_data_mid(window_data_t *wd)
+{
+    memcpy(wd->sort_data, wd->data, wd->size * sizeof(*wd->sort_data));
+    qsort(wd->sort_data, wd->size, sizeof(*wd->sort_data), compare);
+    wd->mid = wd->sort_data[wd->size / 2];
 }
 
 void setup()
 {
-    int32_t values;
-
     //need high baudrate so it won't delay ADC read
     Serial.begin(2000000);
     Serial.println("Pressure bed starting...");
@@ -62,7 +81,8 @@ void setup()
     digitalWrite(LED_DEBUG, LOW);
     digitalWrite(LED_PROBE, LOW);
 
-    window_data_init(&window_data);
+    window_data_init(&window_data, WINDOW_SIZE);
+    window_data_init(&filter_window_data, FILTER_WINDOW_SIZE);
 
     CS1237_init(ADC_SCK, ADC_DOUT);
     if (!CS1237_configure(PGA_128, SPEED_1280, CHANNEL_A))
@@ -71,7 +91,7 @@ void setup()
         Serial.println("ADC configuration failed");
 }
 
-int32_t adc_to_weight(int64_t value)
+int64_t adc_to_weight(int64_t value)
 {
     int64_t weight;
     int64_t con = SENSOR_MAX_VOLT * ADC_MAX_VALUE * ADC_PGA / (ADC_REF_VOLT * SENSOR_MAX_WEIGHT);
@@ -83,26 +103,22 @@ int32_t adc_to_weight(int64_t value)
     return weight;
 }
 
-    int32_t edge_cnt = 0;
 void loop()
 {
-    int32_t ret = 0;
-    int32_t weight = 0;
+    int64_t ret = 0;
+    int64_t weight = 0;
 
     ret = CS1237_read();
     weight = adc_to_weight(ret);
-    window_data_add(&window_data, weight);
+    window_data_add(&filter_window_data, weight);
+    window_data_mid(&filter_window_data);
+    window_data_add(&window_data, filter_window_data.mid);
     if(weight - window_data.mean >= THRESHOLD_STATIC) {
-        edge_cnt++;
-    } else {
-        edge_cnt = 0;
-    }
-    if(edge_cnt >= EDGE_CONTINUOUS) {
         digitalWrite(LED_PROBE, HIGH);
     } else {
         digitalWrite(LED_PROBE, LOW);
     }
     //debug log
-    Serial.printf("%d,%d,%d,%d\n", ret, weight, window_data.mean, edge_cnt ? weight : 0);
+    Serial.printf("%ld,%ld,%ld,%ld\n", (int32_t)ret,(int32_t) weight, (int32_t)filter_window_data.mid, (int32_t)window_data.mean);
     digitalToggle(LED_DEBUG);
 }
